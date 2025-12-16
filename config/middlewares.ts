87bb1f3da30c4d962e6bc9e5a05883581b7b6ecc
NiicolasLencina98 <lenciina.niicolas@gmail.com>
2025-12-16 15:27:45 -0300

    Add pg dependency for Postgresss

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["*"], // para pruebas
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
