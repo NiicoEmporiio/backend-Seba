e8d8f33299b8016a2eef52c45745c5484fb0ffde
NiicolasLencina98 <lenciina.niicolas@gmail.com>
2026-02-09 11:30:55 -0300

    fix: cors configuration for github pages

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,18 +1,26 @@
 export default [
   "strapi::errors",
+
   {
     name: "strapi::cors",
     config: {
       origin: [
         "https://niicoemporiio.github.io",
         "http://localhost:5173",
-        "http://localhost:3000"
+        "http://localhost:3000",
       ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      headers: ["Content-Type", "Authorization"],
-      credentials: true,
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "Origin",
+        "Accept",
+      ],
+      credentials: false,
+      keepHeaderOnError: true,
     },
   },
+
   "strapi::security",
   "strapi::poweredBy",
   "strapi::logger",
