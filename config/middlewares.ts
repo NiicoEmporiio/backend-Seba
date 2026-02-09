9028131e52e2025670f53c369952fd4d2d321214
NiicolasLencina98 <lenciina.niicolas@gmail.com>
2026-02-09 12:44:20 -0300

    fix: cors for admin panel

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,11 @@ export default [
     name: "strapi::cors",
     config: {
       origin: [
-        "https://niicoemporiio.github.io"
+        "https://niicoemporiio.github.io",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
       ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       headers: ["Content-Type", "Authorization"],
